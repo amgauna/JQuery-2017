4b0a1779674168f2b6fff803612e6766b4ba82d1
Ana Gauna <amgauna@gmail.com>
2021-10-28 12:55:18 -0300

    Create aumentar-imagem1.js

---
+++ aumentar-imagem1.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.img-responsive').on("click", function() {
+        $(this).toggleClass('maxSize')
+    });
+});
+
+.maxSize {
+    height: 70%;
+    width: 70%; }
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<br><img class="img-responsive" src="img/teste.png">
