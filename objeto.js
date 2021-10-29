8a681c6d65251d6a31d41067e8cd77830abbcb10
Ana Gauna <amgauna@gmail.com>
2021-10-29 08:09:01 -0300

    Create objeto.js

---
+++ objeto.js
@@ -0,0 +1,5 @@
+// JavaScript também permite acessar propriedades que não estão presentes:
+
+const obj = { width: 10, height: 15 };
+// Why is this NaN? Spelling is hard!
+const area = obj.width * obj.heigth;
