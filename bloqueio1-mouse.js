dc3cfa5b49b113ad0f9c95129128304e64ff5fa8
Ana Gauna <amgauna@gmail.com>
2021-12-01 14:36:16 -0300

    Update bloqueio1-mouse.js

--- bloqueio1-mouse.js
+++ bloqueio1-mouse.js
@@ -1,14 +1,19 @@
 <script type="text/javascript">
+ 
  var t_cel,tc_ln;
- if(document.addEventListener){
-   document.addEventListener("keydown",keyCapt,false);
-   document.addEventListener("keyup",keyCapt,false);
-   document.addEventListener("keypress",keyCapt,false);
- }else{
-   document.attachEvent("onkeydown",keyCapt);
-   document.attachEvent("onkeyup",keyCapt);
-   document.attachEvent("onkeypress",keyCapt);
- }
+ if(document.addEventListener)
+  [
+   document.addEventListener("keydown",keyCapt,false),
+   document.addEventListener("keyup",keyCapt,false),
+   document.addEventListener("keypress",keyCapt,false)
+ ]
+else
+ [
+   document.attachEvent("onkeydown",keyCapt),
+   document.attachEvent("onkeyup",keyCapt),
+   document.attachEvent("onkeypress",keyCapt)
+ ]
+ 
  function keyCapt(e){
    if(typeof window.event!="undefined"){
     var e = (window.event) ? window.event : event;
@@ -22,48 +27,46 @@
     e.preventDefault();
     };
    }else if(e.type=="keyup"){
-   }else if(e.type=="keypress"){﻿
-   }﻿
+   }else if(e.type=="keypress"){}
  }
- </script>﻿
-<script type="text/javascript">﻿
+ 
 function click() {
 if (event.button==2||event.button==3) {
-oncontextmenu='return false';﻿
+oncontextmenu='return false';
 }
 }
-function keypresed() {;﻿
+function keypresed() {;
 }
 document.onmousedown=click
-document.oncontextmenu = new Fun﻿ction("return false;")
-</script>
-<script type='text/javascript'>
-var isCtrl = false;﻿
-document.onkeyup=function(e)﻿
+document.oncontextmenu = new Function("return false;")
+
+var isCtrl = false;
+document.onkeyup=function(e)
 {
-    if(e.which == 17)﻿
-    isCtrl=false;
-}﻿
-document.onkeydown=function(e)
-{﻿
     if(e.which == 17)
-    isCtrl=true;﻿
+    isCtrl=false;
+}
+document.onkeydown=function(e)
+{
+    if(e.which == 17)
+    isCtrl=true;
     if((e.which == 85 || e.which == 83) && (isCtrl == true))
-    {﻿
+    {
         return false;
     }
-}﻿
+}
 var isNS = (navigator.appName == "Chrome") ? 1 : 0;
-if(navigator.appName == "Netscap﻿e") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
+if(navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
 function mischandler(){
-    return false;﻿
+    return false;
 }
-function mousehandler(e){﻿
-    var myevent = (isNS) ? e : e﻿vent;
+function mousehandler(e){
+    var myevent = (isNS) ? e : event;
     var eventbutton = (isNS) ? myevent.which : myevent.button;
-    if((eventbutton==2)||(eventb﻿utton==3)) return false;
+    if((eventbutton==2)||(eventbutton==3)) return false;
 }
-document.oncontextmenu = mischan﻿dler;
-document.onmousedown = mousehand﻿ler;
-document.onmouseup = mousehandle﻿r;
+document.oncontextmenu = mischandler;
+document.onmousedown = mousehandler;
+document.onmouseup = mousehandler;
+
 </script>
